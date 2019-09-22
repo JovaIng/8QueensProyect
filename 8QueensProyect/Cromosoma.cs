1ff731d71cb9e44f6a44e8494f7d84f6ed5feea6
Jovany Hernandez <jovanyhernandez.ing@gmail.com>
2019-09-22 14:03:36 -0700

    Primer subida del proyecto

---
+++ 8QueensProyect/Cromosoma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8QueensProyect
+{
+    public class Cromosoma: IComparable<Cromosoma>
+    {
+        public List<int> Genes{ get; set; }
+        public int Colisiones { get; set; }
+
+        public int Fitness { get; set; }
+        public int Generacion { get; set; }
+        public int Numero { get; set; }
+
+        public Cromosoma()
+        {
+            Genes = new List<int>();
+        }
+
+        public int CompareTo(Cromosoma other)
+        {
+            return (Colisiones.CompareTo(other.Colisiones));
+        }
+    }
+}
